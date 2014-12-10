cb401303e8143631167b597fb44a54c195e82dc3
anaelChardan <anael.chardan@gmail.com>
2014-12-10 11:30:11 +0100

    Création ClassiqueBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Anachardan\Bundle\GeneralBundle\AnachardanGeneralBundle(),
+            new ProjetWeb\ClassiqueBundle\ProjetWebClassiqueBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/ProjetWeb/ClassiqueBundle/ProjetWebClassiqueBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProjetWeb\ClassiqueBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProjetWebClassiqueBundle extends Bundle
+{
+}
