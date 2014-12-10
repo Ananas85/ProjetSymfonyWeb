9f958bbe8f9430fe1140bef79c0bff8304e23041
anaelChardan <anael.chardan@gmail.com>
2014-12-10 11:38:11 +0100

    Création du controller Home

---
+++ src/ProjetWeb/HomeBundle/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 
+ * Created by Sébastien Morel.
+ * Aka: Ananas
+ * Date: 12-10-2014
+ * Time: 11:35
+ * 
+ * Copyright ${PROJECT_AUTHOR} 2014
+ */
+ 
+
+namespace ProjetWeb\HomeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+class HomeController extends Controller {
+
+    public function indexAction() {
+
+    }
+} 
