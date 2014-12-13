2e6564fc160fa8904a43b92d7851f2e59fd326ba
anaelChardan <anael.chardan@gmail.com>
2014-12-13 21:58:45 +0100

    Test distinct

--- src/ProjetWeb/ClassiqueBundle/Controller/CompositeurController.php
+++ src/ProjetWeb/ClassiqueBundle/Controller/CompositeurController.php
@@ -19,20 +19,14 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class CompositeurController extends Controller {
     public function indexAction() {
         $contexte = "Tous";
-        $repoComposer = $this->getDoctrine()->getRepository('ProjetWebClassiqueBundle:Composer');
-        $query = $repoComposer->createQueryBuilder('c')
-                                    ->join('c.codeMusicien','m')
-                                    ->addSelect('m')
-                                    ->distinct('c.codeMusicien')
-                                    ->orderBy('m.nomMusicien','ASC')
-                                    ->getQuery();
-        $resultat = $query->getResult();
-        $compositeur = array();
-        //Methode lente du dictinct mais pour l'instant rien trouvé d'autre
-        foreach($resultat as $compo) {
-            //if (!in_array($compo->getCodeMusicien(),$compositeur))
-                $compositeur[] = $compo->getCodeMusicien();
-        }
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('SELECT distinct m
+                                  FROM ProjetWeb\ClassiqueBundle\Entity\Composer c
+                                  INNER JOIN ProjetWeb\ClassiqueBundle\Entity\Musicien m ON c.codeMusicien = m.codeMusicien
+                                  ORDER BY m.nomMusicien');
+        $compositeur = $query->getResult();
+
 
         return $this->render('ProjetWebClassiqueBundle:Musicien:index.html.twig',array('liste'=>$compositeur, 'contexte'=>$contexte));
     }
