992381ee5413597a6090baa8f8a87b17f9d28454
anaelChardan <anael.chardan@gmail.com>
2014-12-13 20:08:04 +0100

    Test compositeur

---
+++ src/ProjetWeb/ClassiqueBundle/Controller/CompositeurController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 
+ * Created by Sébastien Morel.
+ * Aka: Ananas
+ * Date: 12-13-2014
+ * Time: 19:38
+ * 
+ * Copyright ${PROJECT_AUTHOR} 2014
+ */
+ 
+
+namespace ProjetWeb\ClassiqueBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CompositeurControlleur extends Controller {
+    public function indexAction() {
+        $contexte = "Tous";
+        $repoComposer = $this->getDoctrine()->getRepository('ProjetWebClassiqueBundle:Composer');
+        $query = $repoComposer->createQueryBuilder('c')
+                                    ->join('c.codeMusicien','m')
+                                    ->addSelect('m')
+                                    ->orderBy('m.nomMusicien','ASC');
+        $compositeur = $query->getResult();
+        return $this->render('ProjetWebClassiqueBundle:Musicien:index.html.twig',array('liste'=>$compositeur, 'contexte'=>$contexte));
+    }
+
+    public function initialAction($initial){
+        $contexte = "avec initiale";
+        $repoMusicien = $this->getDoctrine()->getRepository('ProjetWebClassiqueBundle:Musicien');
+        $query = $repoMusicien->createQueryBuilder('m')
+                              ->where('m.nomMusicien LIKE :init')
+                              ->setParameter('init', $initial.'%')
+                              ->orderBy('m.nomMusicien', 'ASC')
+                              ->getQuery();
+        $musicien = $query->getResult();
+        return $this->render('ProjetWebClassiqueBundle:Musicien:index.html.twig',array('liste'=>$musicien, 'contexte'=>$contexte,'initial'=>$initial));
+    }
+} 
