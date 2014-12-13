8a621ceb2f7d6f592a40e49f10be13bc62411ece
anaelChardan <anael.chardan@gmail.com>
2014-12-13 22:06:49 +0100

    Compositeur + distinct mais lent ( too much comparaisons )

--- src/ProjetWeb/ClassiqueBundle/Controller/CompositeurController.php
+++ src/ProjetWeb/ClassiqueBundle/Controller/CompositeurController.php
@@ -19,13 +19,19 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class CompositeurController extends Controller {
     public function indexAction() {
         $contexte = "Tous";
-
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery('SELECT distinct m
-                                  FROM ProjetWeb\ClassiqueBundle\Entity\Composer c
-                                  JOIN ProjetWeb\ClassiqueBundle\Entity\Musicien m');
-        $compositeur = $query->getResult();
-
+        $repoComposer = $this->getDoctrine()->getRepository('ProjetWebClassiqueBundle:Composer');
+        $query = $repoComposer->createQueryBuilder('c')
+                                    ->join('c.codeMusicien','m')
+                                    ->addSelect('m')
+                                    ->orderBy('m.nomMusicien','ASC')
+                                    ->getQuery();
+        $resultat = $query->getResult();
+        $compositeur = array();
+        //Methode lente du dictinct mais pour l'instant rien trouvé d'autre
+        foreach($resultat as $compo) {
+            if (!in_array($compo->getCodeMusicien(),$compositeur))
+                $compositeur[] = $compo->getCodeMusicien();
+        }
 
         return $this->render('ProjetWebClassiqueBundle:Musicien:index.html.twig',array('liste'=>$compositeur, 'contexte'=>$contexte));
     }
